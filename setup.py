d3904657fc8da4f044425f5fd6be0d00ce9fb707
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2016-07-20 14:23:47 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='laminate',
+      version='0.1',
+      description='Tool for creating styled html and pdf from markup',
+      author='Ole Henrik Skogstrøm',
+      author_email='laminate@amplify.no',
+      packages=['laminate', 'laminate.templates'],
+     )
+
+__author__ = 'Ole Henrik Skogstrøm'
